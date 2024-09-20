853d308dbe4a87155c1ad44ca8271aaefa56ce33
phamcongchung <chungpc13122001@gmail.com>
2024-09-20 17:14:50 +0700

    Added GPS config

--- src/main.cpp
+++ src/main.cpp
@@ -1,43 +1,58 @@
 #include <Arduino.h>
 #include <ModbusRTUClient.h>
+#include <SoftwareSerial.h>
 #include <ArduinoRS485.h>
 #include <PubSubClient.h>
+#include <RtcDS3231.h>
+#include <TinyGPS++.h>
 #include <iostream>
 #include <string>
 #include <Wifi.h>
 #include <Wire.h>
 #include <SPI.h>
-#include "RtcDS3231.h"
 #include "FS.h"
 #include "SD.h"
+
+#define GPS_RXD 32
+#define GPS_TXD 33
+#define GPS_BAUD 9600
+
 using namespace std;
 
 RtcDS3231 <TwoWire> Rtc(Wire);
+TinyGPSPlus gps;
+SoftwareSerial gpsSerial(GPS_RXD, GPS_TXD);
+WiFiClient espClient;
+PubSubClient client(espClient);
+
 float volume;
 float ullage;
 char logString[100];
 const char* ssid = "YOUR_WIFI_NAME";
-const char* password = "YOUR_WIFI_PASS";
-const char* brokerUser = "YOUR_BROKER_CLIENT_NAME";
 const char* broker = "YOUR_BROKER_ADDRESS";
-
-WiFiClient espClient;
-PubSubClient client(espClient);
+const char* password = "YOUR_WIFI_PASS";
+const char* brokerUser = "YOUR_BROKER_USER";
 
 void updateLog(const RtcDateTime& dt);
 void appendFile(fs::FS &fs, const char * path, const char * message);
-void testFileIO(fs::FS &fs, const char * path);
 void readFile(fs::FS &fs, const char * path);
+void displayInfo();
 void reconnect();
 
 void setup() {
   // Initialize serial communication
-  Serial.begin(115200); // Or any baud rate your Modbus device uses
+  Serial.begin(115200);
   Serial.print("Compiled: ");
   Serial.print(__DATE__);
   Serial.println(__TIME__);
   delay(500);
 
+  // Initialize SoftwareSerial GPS communication
+  gpsSerial.begin(GPS_BAUD);
+  Serial.println("SoftwareSerial started at 9600 baud rate");
+  delay(500);
+
+  // Initialize WiFi communication
   WiFi.begin(ssid, password);
   Serial.println("Connecting to WiFi");
   while(WiFi.status() != WL_CONNECTED){
@@ -49,6 +64,7 @@ void setup() {
   Serial.println(WiFi.localIP());
   delay (500);
 
+  // Initialize MQTT broker
   client.setServer(broker, 1883);
 
   Rtc.Begin();
@@ -117,9 +133,6 @@ void setup() {
   uint64_t cardSize = SD.cardSize() / (1024 * 1024);
   Serial.printf("SD Card Size: %lluMB\n", cardSize);
 
-  testFileIO(SD, "/test.txt");
-  Serial.printf("Total space: %lluMB\n", SD.totalBytes() / (1024 * 1024));
-  Serial.printf("Used space: %lluMB\n", SD.usedBytes() / (1024 * 1024));
   delay(500);
   // start the Modbus RTU client
   if (!ModbusRTUClient.begin(9600)) {
@@ -135,40 +148,33 @@ void loop() {
     reconnect();
   }
   client.loop();
-  
-  long lastMsg = 0;
-  long now = millis();
-  if(now - lastMsg > 5000){
-    volume = ModbusRTUClient.holdingRegisterRead<float>(1, 0x0036, BIGEND);
-    if (volume < 0) {
-      Serial.print("Failed to read volume: ");
-      Serial.println(ModbusRTUClient.lastError());
-    } else {
-      Serial.print("Volume: ");
-      Serial.println(volume);
-    }
-
-    ullage = ModbusRTUClient.holdingRegisterRead<float>(1, 0x003C, BIGEND);
-    if (ullage < 0) {
-      Serial.print("Failed to read ullage: ");
-      Serial.println(ModbusRTUClient.lastError());
-    } else {
-      Serial.print("Ullage: ");
-      Serial.println(ullage);
-    }
-
-    RtcDateTime now = Rtc.GetDateTime();
-    updateLog(now);
-    client.publish("YOUR_BROKER_TOPIC", logString);
-    readFile(SD, "/log.txt");
+  volume = ModbusRTUClient.holdingRegisterRead<float>(4, 0x0036, BIGEND);
+  if (volume < 0) {
+    Serial.print("Failed to read volume: ");
+    Serial.println(ModbusRTUClient.lastError());
+  } else {
+    Serial.print("Volume: ");
+    Serial.println(volume);
   }
+  ullage = ModbusRTUClient.holdingRegisterRead<float>(4, 0x003C, BIGEND);
+  if (ullage < 0) {
+    Serial.print("Failed to read ullage: ");
+    Serial.println(ModbusRTUClient.lastError());
+  } else {
+    Serial.print("Ullage: ");
+    Serial.println(ullage);
+  }
+
+  RtcDateTime now = Rtc.GetDateTime();
+  updateLog(now);
+  delay(5000);
 }
 
 void reconnect(){
   while(!client.connected()){
     Serial.print("Connecting to ");
     Serial.println(broker);
-    if(client.connect("YOUR_CLIENT_ID", brokerUser, NULL)){
+    if(client.connect("YOUR_BROKER_CLIENT", brokerUser, NULL)){
       Serial.print("Connected to ");
       Serial.println(broker);
     } else{
@@ -195,7 +201,11 @@ void updateLog(const RtcDateTime& dt){
             dt.Second());
   snprintf(logString, sizeof(logString), "%s %s\nVolume: %.1f(L)\nUllage: %.1f(L)\n",
            datestring, timestring, volume, ullage);
-  appendFile(SD, "/log.txt", logString);
+           
+  if(volume >= 0, ullage >= 0){
+    appendFile(SD, "/log.txt", logString);
+    client.publish("YOUR_TOPIC", logString);
+  }
 }
 
 void appendFile(fs::FS &fs, const char * path, const char * message){
@@ -234,44 +244,55 @@ void readFile(fs::FS &fs, const char * path){
   file.close();
 }
 
-void testFileIO(fs::FS &fs, const char * path){
-  File file = fs.open(path);
-  static uint8_t buf[512];
-  size_t len = 0;
-  uint32_t start = millis();
-  uint32_t end = start;
-  if(file){
-    len = file.size();
-    size_t flen = len;
-    start = millis();
-    while(len){
-      size_t toRead = len;
-      if(toRead > 512){
-        toRead = 512;
-      }
-      file.read(buf, toRead);
-      len -= toRead;
-    }
-    end = millis() - start;
-    Serial.printf("%u bytes read for %u ms\n", flen, end);
-    file.close();
-  } else {
-    Serial.println("Failed to open file for reading");
+void displayInfo()
+{
+  Serial.print(F("Satellites: "));
+  Serial.println(gps.satellites.value());
+  Serial.print(F("Location: ")); 
+  if (gps.location.isValid())
+  {
+    Serial.print(gps.location.lat(), 6);
+    Serial.print(F(","));
+    Serial.print(gps.location.lng(), 6);
+  }
+  else
+  {
+    Serial.print(F("INVALID"));
   }
 
-
-  file = fs.open(path, FILE_WRITE);
-  if(!file){
-    Serial.println("Failed to open file for writing");
-    return;
+  Serial.print(F("  Date/Time: "));
+  if (gps.date.isValid())
+  {
+    Serial.print(gps.date.month());
+    Serial.print(F("/"));
+    Serial.print(gps.date.day());
+    Serial.print(F("/"));
+    Serial.print(gps.date.year());
+  }
+  else
+  {
+    Serial.print(F("INVALID"));
   }
 
-  size_t i;
-  start = millis();
-  for(i=0; i<2048; i++){
-    file.write(buf, 512);
+  Serial.print(F(" "));
+  if (gps.time.isValid())
+  {
+    if (gps.time.hour() < 10) Serial.print(F("0"));
+    Serial.print(gps.time.hour());
+    Serial.print(F(":"));
+    if (gps.time.minute() < 10) Serial.print(F("0"));
+    Serial.print(gps.time.minute());
+    Serial.print(F(":"));
+    if (gps.time.second() < 10) Serial.print(F("0"));
+    Serial.print(gps.time.second());
+    Serial.print(F("."));
+    if (gps.time.centisecond() < 10) Serial.print(F("0"));
+    Serial.print(gps.time.centisecond());
   }
-  end = millis() - start;
-  Serial.printf("%u bytes written for %u ms\n", 2048 * 512, end);
-  file.close();
+  else
+  {
+    Serial.print(F("INVALID"));
+  }
+
+  Serial.println();
 }
